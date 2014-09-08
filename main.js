903b948b22202438c925cc4e9e049c7027d749b3
kalynbeach <kalynbeach@gmail.com>
2014-09-08 16:19:03 -0700

    Working shift input and display

--- main.js
+++ main.js
@@ -1,33 +1,51 @@
 /*
 **	Scheduler
 **
-**	An app for keeping track of a weekly work schedule along with total hours worked
+**	An app for keeping track of a weekly work schedule
 **	
 **	TODO:
-**  - 
+**  - Need to make Add Shift button call all needed functions for the newly added shift object
 **	- Add Shift button, it needs to:
-**		- Locate the proper div in the DOM based on the week and day values
-**		- Display the start time and end time values in the located div
+**		- DONE: Locate the proper div in the DOM based on the week and day values
+**		- DONE: Display the start time and end time values in the located div
 */
 
 // ==========================
 //	Objects and Construction
 // ==========================
 
-// Shift Constructor
+// Array of shift objects
+var shifts = [];
+
+// Shift object Constructor
 var Shift = function(week, day, start, end) {
 	this.week = week;
 	this.day = day;
 	this.start = start;
 	this.end = end;
 
+	// Return the shift
 	this.returnShift = function() {
 		return this;
+	};
+
+	// Call displayShift function on the shift
+	this.addToSchedule = function() {
+		displayShift(this);
+	};
+
+	// Remove the shift from the shifts array
+	this.deleteShift = function() {
+		shifts.splice(this, 1);
+	};
+
+	this.logShift = function() {
+		console.log("Week of shift: " + this.week);
+		console.log("Day of shift: " + this.day);
+		console.log("Start time: " + this.start);
+		console.log("End time: " + this.end);
 	}
 };
-
-// Array of shift objects
-var shifts = [];
 
 //
 // ====================================
@@ -38,17 +56,23 @@ var shifts = [];
 // ================
 
 
-// Create shift object from gatherInputValues() values
-function createShift(week, day, start, end) {
+// Create shift object from gatherInputValues() values 
+// == function needs to be cleaned up and separated ==
+function createShiftObject(week, day, start, end) {
 	var shift = new Shift(week, day, start, end);
   shifts.push(shift);
+
+  // Display shift by adding it to the DOM
+  displayShift(shift);
 };
 
 // Gather form input values
 function gatherInputValues() {
 
-		var options = $('input[name=options]'); // Radio button elements
+		// Radio button elements
+		var options = $('input[name=options]'); 
 
+		// Value variables
 		var week;
 		var day;
 		var start;
@@ -98,13 +122,9 @@ function gatherInputValues() {
 	gatherStartValue();
 	gatherEndValue();
 
-	// Call createShift function on newly returned values
-	createShift(week, day, start, end);
+	// Call createShiftObject function on newly returned values
+	createShiftObject(week, day, start, end);
 
-	console.log("Week of shift: " + week);
-	console.log("Day of shift: " + day);
-	console.log("Start time: " + start);
-	console.log("End time: " + end);
 };
 
 // Console.log() a shift object's values
@@ -115,9 +135,83 @@ function logShift(shift) {
 	console.log("End time: " + shift.end);
 };
 
-// Find proper html element to display values in
+// Find proper div to display values in, then insert values into the div
 function displayShift(shift) {
 
+	// Assign shift values to vars
+	var week  = shift.week;
+	var day   = shift.day;
+	var start = shift.start;
+	var end   = shift.end;
+
+	// Div locations
+	var weekDiv;
+	var timesDiv;
+
+	// Select proper week div
+	switch (week) {
+		case "This Week":
+			weekDiv = "#this-week-section";
+			break;
+		case "Next Week":
+			weekDiv = "#next-week-section";
+			break;
+	};
+
+	// Select proper day div
+	if (weekDiv === "#this-week-section") {
+		switch (day) {
+			case "Monday":
+				timesDiv = "#this-mon-times";
+				break;
+			case "Tuesday":
+				timesDiv = "#this-tue-times";
+				break;
+			case "Wednesday":
+				timesDiv = "#this-wed-times";
+				break;
+			case "Thursday":
+				timesDiv = "#this-thu-times";
+				break;
+			case "Friday":
+				timesDiv = "#this-fri-times";
+				break;
+			case "Saturday":
+				timesDiv = "#this-sat-times";
+				break;
+			case "Sunday":
+				timesDiv = "#this-sun-times";
+				break;
+		}
+	}	else if (weekDiv === "#next-week-section") {
+		switch (day) {
+			case "Monday":
+				timesDiv = "#next-mon-times";
+				break;
+			case "Tuesday":
+				timesDiv = "#next-tue-times";
+				break;
+			case "Wednesday":
+				timesDiv = "#next-wed-times";
+				break;
+			case "Thursday":
+				timesDiv = "#next-thu-times";
+				break;
+			case "Friday":
+				timesDiv = "#next-fri-times";
+				break;
+			case "Saturday":
+				timesDiv = "#next-sat-times";
+				break;
+			case "Sunday":
+				timesDiv = "next-sun-times";
+				break;
+		}
+	}
+
+	// Insert time strings to the proper div
+	$(timesDiv).text(start + " - " + end);
+	
 };
 
 //
@@ -132,11 +226,13 @@ function displayShift(shift) {
 $(document).ready(function() {
 
 	// Add Shift button behavior
-	$('#new-task-form').submit(function() {
+	$('#new-task-form').submit(function(e) {
 
 		gatherInputValues();
 
-		return false;
+		e.preventDefault();
+
+		//return false;
 	});
 
 }); 
